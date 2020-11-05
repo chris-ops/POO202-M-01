ba72efd1a972024436294e209fb892afce52145f
yagakimi <yagakimi@DESKTOP-DC5I3IA>
2020-11-05 19:24:35 -0300

    prototipo

---
+++ LogicaPredicados.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class LogicaPredicados extends Enigma{
+
+	public LogicaPredicados(int qtdAtivacoes, int qtdUsos, int qtdDecifrados, int qtdErros, String descricao,
+			HashMap<Boolean, Alternativa> alternativas) {
+		super(qtdAtivacoes, qtdUsos, qtdDecifrados, qtdErros, descricao, alternativas);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+
+}
